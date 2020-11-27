5068fce11e4cf695e6927a7fef0e715478465866
dilexet <63055058+dilexet@users.noreply.github.com>
2020-11-27 21:11:31 +0300

    Implemented PlayList

--- Task_0/MediaLibrary/Media/PLayList.cs
+++ Task_0/MediaLibrary/Media/PLayList.cs
@@ -1,7 +1,49 @@
-﻿namespace MediaLibrary.Media
+﻿using System.Collections.Generic;
+using MediaLibrary.Interfaces;
+using MediaLibrary.MediaFiles;
+
+namespace MediaLibrary.Media
 {
-    public class PLayList
+    internal class PlayList : IPlayList
     {
+        protected string Name;
+
+        protected ICollection<MediaFile> mediaFiles;
+
+        public PlayList(string name)
+        {
+            Name = name;
+            mediaFiles = new List<MediaFile>();
+        }
         
+        public void AddFile(MediaFile mediaFile)
+        {
+            mediaFiles.Add(mediaFile);
+        }
+
+        public void RemoveFile(MediaFile mediaFile)
+        {
+            mediaFiles.Remove(mediaFile);
+        }
+
+        public void ClearAllFiles()
+        {
+            mediaFiles.Clear();
+        }
+
+        public MediaFile SearchFile(string name)
+        {
+            foreach (var mediaFile in mediaFiles)
+            {
+                if (mediaFile.Name.Contains(name))
+                    return mediaFile;
+            }
+            return null;
+        }
+        
+        public ICollection<MediaFile> GetMediaFiles()
+        {
+            return mediaFiles;
+        }
     }
 }
