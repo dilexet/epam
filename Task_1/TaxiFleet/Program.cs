c067ee1d7f84be491487d0e8b736054038483085
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-01 21:39:23 +0300

    Changed the parameters passed to the constructor

--- Task_1/TaxiFleet/Program.cs
+++ Task_1/TaxiFleet/Program.cs
@@ -8,19 +8,24 @@ namespace TaxiFleet
     {
         public static void Main(string[] args)
         {
+            MockCategoryTaxi mockCategoryTaxi = new MockCategoryTaxi();
             ICollection<Car> cars = new List<Car>()
             {
                 new CargoTaxi(CarBrands.Peugeot, "Boxer Van 3", BodyTypes.Van,
-                    "7582-DN", CarColors.Red, 11000, 2.8f, 210, 2000, TaxiClasses.Cargo, 15f),
+                    "7582-DN", CarColors.Red, 11000, 2.8f, 210, 2000, mockCategoryTaxi.GetCategory(TaxiClasses.Cargo), 15f),
+                
                 new PassengerTaxi(CarBrands.Bmw, "X6", BodyTypes.Crossover,
-                    "8734-KC", CarColors.Black, 24000, 1.8f, 240, 2015, TaxiClasses.Comfort),
+                    "8734-KC", CarColors.Black, 24000, 1.8f, 240, 2015, mockCategoryTaxi.GetCategory(TaxiClasses.Comfort)),
+                
                 new PassengerTaxi(CarBrands.Volkswagen, "Caddy 5", BodyTypes.Minivan,
-                    "6523-LV", CarColors.Green, 15000, 2.4f, 220, 2005, TaxiClasses.Compactvan)
+                    "6523-LV", CarColors.Green, 15000, 2.4f, 220, 2005, mockCategoryTaxi.GetCategory(TaxiClasses.Compactvan))
             };
             TaxiPark taxiPark = new TaxiPark(cars);
+            //taxiPark.PrintInfo();
             // taxiPark.TotalRevenueOfTaxiPark();
             Customer person1 = new Customer("Maksim");
             person1.TaxiOrdering(taxiPark);
+            
         }
     }
 }
