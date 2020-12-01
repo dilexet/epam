5e2df167248eeff3d5cb0b337d1602138e8cebaf
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-01 21:35:20 +0300

    Added new classes: "CategoryTaxi" and "MockCategoryTaxi"

---
+++ Task_1/TaxiFleet/Category/CategoryTaxi.cs
@@ -0,0 +1,17 @@
+﻿using TaxiFleet.Enums;
+using TaxiFleet.Taxi;
+
+namespace TaxiFleet
+{
+    internal class CategoryTaxi
+    {
+        public readonly TaxiClasses TaxiClass;
+        public readonly string Description;
+
+        public CategoryTaxi(TaxiClasses taxiClass, string description)
+        {
+            TaxiClass = taxiClass;
+            Description = description;
+        }
+    }
+}
