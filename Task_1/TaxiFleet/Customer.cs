23f09be38f023351f8da976801749be6b6077263
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-01 21:39:28 +0300

    Update Customer.cs

--- Task_1/TaxiFleet/Customer.cs
+++ Task_1/TaxiFleet/Customer.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Threading;
 using TaxiFleet.Taxi;
 
 namespace TaxiFleet
 {
     // сделать: при заказе выбор класса такси, вывод суммы к оплате, исходя из растояния(генерить рандомно)
+    // сделать: разбитьь функцию TaxiOrdering на несколько маленьких
     internal class Customer
     {
         private string _name;
-        private string RouteStartAddress;
-        private string RouteEndAddress;
+        private string _routeStartAddress;
+        private string _routeEndAddress;
         public Customer(string name)
         {
             _name = name;
@@ -17,9 +19,10 @@ namespace TaxiFleet
         public void TaxiOrdering(TaxiPark taxiPark)
         {
             Console.Write("Enter your address: ");
-            RouteStartAddress = Console.ReadLine();
+            _routeStartAddress = Console.ReadLine();
             Console.Write("Where we go: ");
-            RouteEndAddress = Console.ReadLine();
+            _routeEndAddress = Console.ReadLine();
+            
             Random random = new Random();
             PassengerTaxi taxi;
             do
@@ -29,6 +32,13 @@ namespace TaxiFleet
 
             Console.WriteLine(
                 $"In {random.Next(1, 15)} minute {taxi.Brand} {taxi.Model} will arrive, color {taxi.CarColor}, number {taxi.CarRegistrationNumber}");
+            Thread.Sleep(5000);
+            Console.Clear();
+            Console.WriteLine(
+                $"{_name} is waiting for you {taxi.Brand} {taxi.Model}\n" +
+                $"{_routeStartAddress} - {_routeEndAddress}\n" +
+                $"To pay {taxi.CostOfTrip * random.Next(1, 40)}$\n");
+
         }
     }
 }
