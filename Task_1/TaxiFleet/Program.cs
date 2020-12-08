4d8c9020d8bcd410fa258d7397e1a02f612fc1df
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-08 20:33:46 +0300

    Update Program.cs

--- Task_1/TaxiFleet/Program.cs
+++ Task_1/TaxiFleet/Program.cs
@@ -1,26 +1,17 @@
-﻿using TaxiFleet.Taxi;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TaxiFleet.Enums;
+using TaxiFleet.Mocks;
+using TaxiFleet.Models;
 
 namespace TaxiFleet
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            TaxiPark taxiPark = new TaxiPark();
-            taxiPark.Print();
-            //taxiPark.PrintInfo();
-            // taxiPark.TotalRevenueOfTaxiPark();
-            // Customer person1 = new Customer("Maksim");
-            // person1.TaxiOrdering(taxiPark);
-            // taxiPark.SortingByFuelConsumption();
-            // var car = taxiPark.SelectSpeedTaxi();
-            
-            /*
-            foreach (var item in car)
-            {
-                item.PrintInfo();
-            }
-            */
             
         }
     }
