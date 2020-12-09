afdf6aeceeec08933230bbd5eb6bf68361c0933b
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-10 02:46:00 +0300

    Update TaxiFleetTests.cs

--- Task_1/TaxiFleet.UnitTests/TaxiFleetTests.cs
+++ Task_1/TaxiFleet.UnitTests/TaxiFleetTests.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using NUnit.Framework;
-using TaxiFleet.Library.Enums;
 using TaxiFleet.Library.Models;
 using TaxiFleet.UnitTests.Mocks;
 
@@ -16,11 +14,11 @@ namespace TaxiFleet.UnitTests
             // arrange
             MockCars mockCars = new MockCars();
             TaxiStation taxiStation = new TaxiStation(mockCars.GetCars);
-            
+
             double expected = 94000;
 
             // act
-            
+
             double actual = taxiStation.TaxiFleetCost();
 
             // assert
@@ -31,7 +29,7 @@ namespace TaxiFleet.UnitTests
         [Test]
         public void SelectSpeedTaxiTests()
         {
-            
+
             // arrange
             MockCars mockCars = new MockCars();
             TaxiStation taxiStation = new TaxiStation(mockCars.GetCars);
@@ -49,44 +47,19 @@ namespace TaxiFleet.UnitTests
         [Test]
         public void SortingByFuelConsumptionTests()
         {
-            
+
             // arrange
             MockCars mockCars = new MockCars();
-            
             TaxiStation taxiStation = new TaxiStation(mockCars.GetCars);
-            MockCategoryTaxi mockCategoryTaxi = new MockCategoryTaxi();
-            
-            var expected = new List<CarBase>
-            {
-                new PassengerTaxi(CarBrand.Bmw, "X6", CarBody.Crossover,
-                    "8734-KC", CarColor.Black, 24000, 1.8f, 220, 2015, 
-                    mockCategoryTaxi.GetCategory(TaxiClass.Comfort), 4, 3.92f),
-               
-                new PassengerTaxi(CarBrand.Volkswagen, "Caddy 5", CarBody.Minivan,
-                    "6523-LV", CarColor.Green, 15000, 2.4f, 200, 2005, 
-                    mockCategoryTaxi.GetCategory(TaxiClass.Compactvan), 6, 5.79f),
-              
-                new PassengerTaxi(CarBrand.Audi, "A3", CarBody.Hatchback, 
-                    "8361-PB", CarColor.Blue, 30000, 3.6f, 210, 2016, 
-                    mockCategoryTaxi.GetCategory(TaxiClass.Business), 4, 8.17f),
-              
-                new CargoTaxi(CarBrand.Citroen,"Jumpy 3", CarBody.Minivan, 
-                    "9274-MP", CarColor.White, 14000, 5.1f, 140, 2016, 
-                    mockCategoryTaxi.GetCategory(TaxiClass.Cargo), 10f, 8.5f),
-              
-                new CargoTaxi(CarBrand.Peugeot, "Boxer Van 3", CarBody.Van,
-                    "7582-DN", CarColor.Red, 11000, 5.3f, 150, 2000, 
-                    mockCategoryTaxi.GetCategory(TaxiClass.Cargo), 15f, 12.5f),
-            };
-            
+            FakeMockCars fakeMockCars = new FakeMockCars();
+            var expected = fakeMockCars.GetCars.ToList();
+
             // act
             var actual = taxiStation.SortingByFuelConsumption().ToList();
 
             // assert
-            Assert.AreEqual(expected, actual);
-            
+            Assert.That(expected, Is.EqualTo(actual).Using(new EqualityComparer()));
         }
     }
 }
-
  
