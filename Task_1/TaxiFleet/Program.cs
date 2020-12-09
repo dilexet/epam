a15f6f304b9446e0f28bed40ba479c364c248aa6
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-09 17:02:25 +0300

    Update Program.cs

--- Task_1/TaxiFleet/Program.cs
+++ Task_1/TaxiFleet/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using TaxiFleet.Enums;
-using TaxiFleet.Mocks;
-using TaxiFleet.Models;
-
-namespace TaxiFleet
+﻿namespace TaxiFleet
 {
     public class Program
     {
