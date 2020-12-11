2c0e9ffeed67c367a3bc3c25dee2485bdf84b6ba
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-11 11:51:15 +0300

    Moved files

---
+++ Task_1/TaxiFleet.Library/Mocks/MockCars.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using TaxiFleet.Library.Enums;
+using TaxiFleet.Library.Models;
+
+namespace TaxiFleet.Library.Mocks
+{
+    public class MockCars
+    {
+        private readonly MockCategoryTaxi _mockCategoryTaxi = new MockCategoryTaxi();
+
+        public IEnumerable<CarBase> GetCars =>
+            new List<CarBase>
+            {
+                new CargoTaxi(CarBrand.Peugeot, "Boxer Van 3", CarBody.Van,
+                    "7582-DN", CarColor.Red, 11000, 5.3f, 150, 2000, 
+                    _mockCategoryTaxi.GetCategory(TaxiClass.Cargo), 15f, 12.5f),
+                
+                new PassengerTaxi(CarBrand.Bmw, "X6", CarBody.Crossover,
+                    "8734-KC", CarColor.Black, 24000, 1.8f, 220, 2015, 
+                    _mockCategoryTaxi.GetCategory(TaxiClass.Comfort), 4, 3.92f),
+                
+                new PassengerTaxi(CarBrand.Volkswagen, "Caddy 5", CarBody.Minivan,
+                    "6523-LV", CarColor.Green, 15000, 2.4f, 200, 2005, 
+                    _mockCategoryTaxi.GetCategory(TaxiClass.Compactvan), 6, 5.79f),
+                    
+                new CargoTaxi(CarBrand.Citroen,"Jumpy 3", CarBody.Minivan, 
+                    "9274-MP", CarColor.White, 14000, 5.1f, 140, 2016, 
+                    _mockCategoryTaxi.GetCategory(TaxiClass.Cargo), 10f, 8.5f),
+                    
+                new PassengerTaxi(CarBrand.Audi, "A3", CarBody.Hatchback, 
+                    "8361-PB", CarColor.Blue, 30000, 3.6f, 210, 2016, 
+                    _mockCategoryTaxi.GetCategory(TaxiClass.Business), 4, 8.17f),
+                    
+            };
+    }
+}
---
+++ Task_1/TaxiFleet.Library/Mocks/MockCategoryTaxi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TaxiFleet.Library.Enums;
+using TaxiFleet.Library.Models;
+
+namespace TaxiFleet.Library.Mocks
+{
+    public class MockCategoryTaxi
+    {
+        private IEnumerable AllCategoriesTaxi =>
+            new List<CategoryTaxi>
+            {
+                new CategoryTaxi(TaxiClass.Economy, "Balance of price and quality"),
+                new CategoryTaxi(TaxiClass.Comfort, "High level of convenience"),
+                new CategoryTaxi(TaxiClass.Compactvan, "Small company or large luggage"),
+                new CategoryTaxi(TaxiClass.Business, "Spacious interior and attention to detail"),
+                new CategoryTaxi(TaxiClass.Minivan, "Plenty of space for a friendly company and luggage"),
+                new CategoryTaxi(TaxiClass.Cargo, "Fast delivery of any cargo")
+            };
+
+        public CategoryTaxi GetCategory(TaxiClass taxiClass)
+        {
+            foreach (var category in AllCategoriesTaxi)
+            {
+                if (category is CategoryTaxi categoryTaxi && categoryTaxi.TaxiClass == taxiClass) 
+                    return categoryTaxi;
+            }
+            return null;
+        }
+    }
+}
