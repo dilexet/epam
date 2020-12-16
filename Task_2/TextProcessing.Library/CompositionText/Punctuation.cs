695c6dbfe3a1c41246ba75de7ad762b1fa8fdfaa
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-16 22:46:28 +0300

    Created a class hierarchy and text parsing

---
+++ Task_2/TextProcessing.Library/CompositionText/Punctuation.cs
@@ -0,0 +1,20 @@
+﻿namespace TextProcessing.Library.CompositionText
+{
+    public class Punctuation
+    {
+        private Symbol _symbol;
+
+        public Punctuation(Symbol symbol)
+        {
+            _symbol = symbol;
+        }
+
+        public string Chars
+        {
+            get
+            {
+                return _symbol.Chars;
+            }
+        }
+    }
+}
---
+++ Task_2/TextProcessing.Library/CompositionText/Text.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TextProcessing.Library.CompositionText
+{
+    public class Text
+    {
+        private ICollection<Sentence> _sentences;
+
+        public Text(ICollection<Sentence> sentences)
+        {
+            _sentences = sentences;
+        }
+
+        public IEnumerable<string> GetEnumerator()
+        {
+            foreach (var sentence in _sentences)
+            {
+                yield return sentence.GetSentence();
+            }
+        }
+
+        public IEnumerable<Sentence> SortByTheNumberOfWordsInASentence()
+        {
+            return _sentences.OrderBy(item => item.WordsCount).ToList();
+        }
+
+        private IEnumerable<Sentence> ReceivingInterrogativeSentences()
+        {
+            return _sentences.Where(sentence => sentence.GetSentence().EndsWith("?")).ToList();
+        }
+
+        private IEnumerable<Word> RemovingDuplicateItems(List<Word> words)
+        {
+            for (int i = 0; i < words.Count; i++)
+            {
+                for (int j = i + 1; j < words.Count; j++)
+                {
+                    if (words[i].Chars == words[j].Chars)
+                        words.Remove(words[j]);
+                }
+            }
+            return words;
+        }
+        public IEnumerable<Word> FetchingWordsOfAGivenLength(uint length)
+        {
+            List<Word> words = new List<Word>();
+            foreach (var sentence in ReceivingInterrogativeSentences())
+            {
+                words = sentence.Words.Where(word => word.SymbolCount == length).ToList();
+            }
+
+            return RemovingDuplicateItems(words);
+        }
+    }
+}
