c4b34d25405b38ac3021104eaff4237ca99f8019
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-20 00:16:46 +0300

    Create IParser.cs

---
+++ Task_2/TextProcessing.Library/Interfaces/IParser.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using TextProcessing.Library.CompositionText;
+
+namespace TextProcessing.Library.Interfaces
+{
+    public interface IParser
+    {
+        Text Parse(FileStream fileStream);
+    }
+}
