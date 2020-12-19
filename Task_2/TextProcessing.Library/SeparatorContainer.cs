350b2c1eb8c2fd5098c921808ab47f1f5b4ad6f0
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-20 00:16:54 +0300

    Create SeparatorContainer.cs

---
+++ Task_2/TextProcessing.Library/SeparatorContainer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TextProcessing.Library
+{
+    public class SeparatorContainer
+    {
+        private readonly string[] _wordSeparators = {",", ";", "\"", "'", "\n", " ", "—", "-", ":"};
+        private readonly string[] _sentenceSeparators = {".", "!", "?", "?!", "!?", "..."};
+        public IEnumerable<string> SentenceSeparators()
+        {
+            return _sentenceSeparators.AsEnumerable();
+        }
+        public IEnumerable<string> WordSeparators()
+        {
+            return _wordSeparators.AsEnumerable();
+        }
+    }
+}
