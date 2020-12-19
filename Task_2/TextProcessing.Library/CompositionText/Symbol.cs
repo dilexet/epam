174c2aa7957d583037196e8e6a0dbdf6f0f7ac3d
dilexet <63055058+dilexet@users.noreply.github.com>
2020-12-20 00:17:39 +0300

    Update Symbol.cs

--- Task_2/TextProcessing.Library/CompositionText/Symbol.cs
+++ Task_2/TextProcessing.Library/CompositionText/Symbol.cs
@@ -1,23 +1,21 @@
-﻿using System;
-
-namespace TextProcessing.Library.CompositionText
+﻿namespace TextProcessing.Library.CompositionText
 {
     public class Symbol
     {
         public string Chars { get; }
-
         private readonly char[] _vowelList = { 'a', 'A', 'e', 'E', 'u', 
             'U', 'i', 'I', 'o', 'O', 'а', 'А', 'е', 'Е', 'ё', 'Ё', 'и', 
             'И', 'о', 'О', 'у', 'У', 'э', 'Э', 'ы', 'Ы', 'ю', 'Ю', 'я', 'Я' };
-
-        public bool IsConconat
+        public bool IsConsonant
         {
             get
             {
                 foreach (var letter in _vowelList)
                 {
                     if (Chars == letter.ToString())
+                    {
                         return false;
+                    }
                 }
                 return true;
             }
@@ -26,21 +24,9 @@ namespace TextProcessing.Library.CompositionText
         {
             Chars = chars;
         }
-        
         public Symbol(char source)
         {
-            Chars = String.Format("{0}", source);
+            Chars = $"{source}";
         }
-
-        // public bool IsUppercase
-        // {
-        //     get { return chars != null && chars.Length >= 1 && char.IsLetter(chars[0]) && char.IsUpper(chars[0]); }
-        // }
-        //
-        // public bool IsLower 
-        // { 
-        //     get { return chars != null && chars.Length >= 1 && char.IsLetter(chars[0]) && char.IsLower(chars[0]); } 
-        // }
-
     }
 }
